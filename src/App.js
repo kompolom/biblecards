881b08b6cc3d97c8f6bd906f9e3a84fd665e75d6
Dmitriy Panzhinskiy <pandimun@gmail.com>
2020-10-15 21:29:21 +0300

    first crash

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +18,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         ПРивет!! 
         </a>
       </header>
         <Button>Click Me!</Button>
