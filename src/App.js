712cd102fb101d2d7858f4d89b31409df5335b6a
Dmitriy Panzhinskiy <pandimun@gmail.com>
2020-10-20 23:21:45 +0300

    1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-         Text for you
+        <p className="text-p">
+        Эфесянам 5:33
         </p>
       </header>
+
     </div>
   );
 }
