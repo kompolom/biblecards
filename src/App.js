280ed6faebb80408d10a8acdf5a2fe876d58be28
Dmitriy Panzhinskiy <pandimun@gmail.com>
2020-10-21 18:51:27 +0300

    top-app-bar. ver.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@material/react-top-app-bar/dist/top-app-bar.css';
+import '@material/react-material-icon/dist/material-icon.css';
+import TopAppBar, {
+  TopAppBarFixedAdjust, 
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from '@material/react-top-app-bar';
+import MaterialIcon from '@material/react-material-icon';
 
+function MyComponent() {
+  return (
+    <div>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection align='start'>
+            <TopAppBarIcon navIcon tabIndex={0}>
+              <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')} />
+            </TopAppBarIcon>
+            <TopAppBarTitle>Процитируй стих</TopAppBarTitle>
+          </TopAppBarSection>
+          <TopAppBarSection align='end' role='toolbar'>
+            <TopAppBarIcon actionItem tabIndex={0}>
+              <MaterialIcon
+                aria-label="print page"
+                hasRipple
+                icon='print'
+                onClick={() => console.log('print')} />
+            </TopAppBarIcon>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <TopAppBarFixedAdjust>
+        My exciting content!
+      </TopAppBarFixedAdjust>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
