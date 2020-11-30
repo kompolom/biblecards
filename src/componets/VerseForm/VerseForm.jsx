45595ddc0d7a6a6ac914a6d5e82b138f2ab6f2ec
Evgeniy Baranov <master@kompolom.ru>
2020-11-30 19:41:02 +0400

    Начало формы

---
+++ src/componets/VerseForm/VerseForm.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Textfield } from '@material-ui/core'
+
+export const VerseForm = (props) => {
+    return (<form>
+        <Textfield label="Глава"/>
+    </form>);
+}
