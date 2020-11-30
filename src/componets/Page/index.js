9755466c962a32b4bb07480ecdfefeae2a656da6
Dmitriy Panzhinskiy <pandimun@gmail.com>
2020-11-30 17:08:25 +0300

    Toolbars1

--- src/componets/Page/index.js
+++ src/componets/Page/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-//export const Page = () => <div className="Page"></div>
-function Page(props) {
-    return React.createElement('div', { className: "Page", style: { 'textAlign': 'left', color: props.color } }, props.children);
-}
+export const Page = (props) => <div className="Page">
+    {props.children}
+</div>
 
 export default Page;
