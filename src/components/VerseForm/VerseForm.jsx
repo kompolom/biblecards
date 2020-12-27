3e463dabc4642022397d4f3b30f24fa4a30362d6
Dmitriy Panzhinskiy <pandimun@gmail.com>
2020-12-27 14:50:25 +0300

    Доработка стилей формы

--- src/components/VerseForm/VerseForm.jsx
+++ src/components/VerseForm/VerseForm.jsx
@@ -19,16 +19,37 @@ export const VerseForm = (props) => {
             }}
         >
             <Form className="container" >
-                <div className="VerseForm-row">
-                    <Field as="select" name="listBooks">
-                        { books.map(book => { return ( <option key={book}> {book} </option> ) } ) }
+                <div className="VerseForm-body VerseForm-row">
+                    <Field 
+                        className="VerseForm-row" 
+                        as="select" 
+                        name="listBooks">
+                            { books.map(book => { return ( <option key={book}> {book} </option> ) } ) }
                     </Field>
-                    <Field id="chaperForm" name="chapter" type="number" min="1" max="150" placeholder="Глава"/>
-                    <Field id="verseForm" name="verse" type="number" min="1" placeholder="Стих" required />
-                    <Field id="textForm" name="text" placeholder="Текст" required />
+                    <Field 
+                        className="VerseForm-row" 
+                        id="chaperForm" 
+                        name="chapter" 
+                        type="number" 
+                        min="1" max="150" 
+                        placeholder="Глава"/>
+                    <Field 
+                        className="VerseForm-row"
+                        id="verseForm" 
+                        name="verse" 
+                        type="number" 
+                        min="1" 
+                        placeholder="Стих" required />
+                    <Field 
+                        className="VerseForm-row"
+                        id="textForm" 
+                        name="text" 
+                        placeholder="Текст" required />
                 </div>
                 <div className="VerseForm-row VerseForm-SubmitSection">
-                    <Button type="submit" variant="contained" color="primary">Добавить</Button>
+                    <Button type="submit" variant="contained" color="primary">
+                        Добавить
+                    </Button>
                 </div>
             </Form>
         </Formik>
