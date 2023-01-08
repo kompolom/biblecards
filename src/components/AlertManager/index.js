71b8d8ac04d4249f4fed628a4715bff947fe48c6
Evgeniy Baranov <master@kompolom.ru>
2023-01-08 17:49:26 +0300

    AlertManager use mui@5

--- src/components/AlertManager/index.js
+++ src/components/AlertManager/index.js
@@ -1,32 +1,32 @@
-import React, {
-    createContext,
-    useContext,
-} from 'react';
+import React, { createContext, useContext, useState } from 'react';
+import { Snackbar, Alert } from '@mui/material';
 import { connect } from 'react-redux';
-import { Alert } from "../Alert";
-import './AlertManager.css'
-import {hideAlert, showAlert} from "../../Redux/actions";
+import { hideAlert, showAlert } from '../../Redux/actions';
 
 export const AlertManagerContext = createContext();
-AlertManagerContext.displayName = 'AlertManagerContext'
+AlertManagerContext.displayName = 'AlertManagerContext';
 
 export function useAlertManager() {
-    return useContext(AlertManagerContext);
+  return useContext(AlertManagerContext);
 }
 
 export const AlertManagerProvider = connect(
-    ({ alerts }) => ({ alerts }),
-    (dispatch) => ({
-        showAlert: (t, p) => dispatch(showAlert(t, p)),
-        hideAlert: (id) => dispatch(hideAlert(id))
-    }))
-(({ alerts, ...props}) => {
-    return (
-        <AlertManagerContext.Provider value={props.showAlert}>
-            {props.children}
-            <div className="AlertManager">{alerts.map(alert => {
-                return (<Alert handleClose={props.hideAlert} key={alert.id} {...alert} />)
-            })}</div>
-        </AlertManagerContext.Provider>
-    )
-})
+  ({ alerts }) => ({ alerts }),
+  (dispatch) => ({
+    showAlert: (t, p) => dispatch(showAlert(t, p)),
+    hideAlert: (id) => dispatch(hideAlert(id)),
+  }),
+)(({ alerts, ...props }) => {
+  return (
+    <AlertManagerContext.Provider value={props.showAlert}>
+      {props.children}
+      <Snackbar open={Boolean(alerts.length)} className="AlertManager">
+        {alerts.map((alert) => {
+          return (
+            <Alert onClose={props.hideAlert} key={alert.id} severity={alert.status} {...alert} />
+          );
+        })}
+      </Snackbar>
+    </AlertManagerContext.Provider>
+  );
+});
