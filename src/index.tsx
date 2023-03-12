fc9eb3010a5e2823be6369b204d44e314d12b340
Evgeniy Baranov <master@kompolom.ru>
2023-03-12 22:07:31 +0300

    switch react to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { store } from './Redux/store';
 import { getDb } from './getDb';
-import { loadVerses } from "./Redux/actions";
+import { loadVerses } from './Redux/actions';
 
-
-ReactDOM.render(
-   <Provider store={store}>
-      <App />
-   </Provider>, 
-   document.getElementById('⚛️')
+const root = createRoot(document.getElementById('⚛️'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-
-getDb().then(async db => {
-    const verses = await db.getVerses();
-    store.dispatch(loadVerses(verses));
-})
+getDb().then(async (db) => {
+  const verses = await db.getVerses();
+  store.dispatch(loadVerses(verses));
+});
 
 serviceWorker.unregister();
