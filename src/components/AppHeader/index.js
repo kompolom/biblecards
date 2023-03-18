118329927481d70975bd30b62e6c28d51a15e65c
Evgeniy Baranov <master@kompolom.ru>
2023-03-18 23:04:20 +0300

    prettier AppHeader

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,46 +1,76 @@
 import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton, Typography, SwipeableDrawer, List, ListItem, ListItemText, ListItemButton } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
 import routes from '../../routes';
 
 export const AppHeader = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
   const toggleDrawer = (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
     setDrawerOpen(!isDrawerOpen);
-  }
+  };
   const history = useHistory();
-  const onRouteClick = React.useCallback((e) => {
-    e.preventDefault();
-    history.push(e.currentTarget.getAttribute('href'))
-  }, [history]);
+  const onRouteClick = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      history.push(e.currentTarget.getAttribute('href'));
+    },
+    [history],
+  );
   const pathname = useLocation().pathname;
-  const currentRoute = routes.find(route => route[0] === pathname);
-  const pageTitle = currentRoute? currentRoute[1]: 'Редактировать';
+  const currentRoute = routes.find((route) => route[0] === pathname);
+  const pageTitle = currentRoute ? currentRoute[1] : 'Редактировать';
 
-    return (<React.Fragment>
+  return (
+    <React.Fragment>
       <AppBar position="sticky">
         <Toolbar>
-          <IconButton onClick={toggleDrawer} edge="start" color="inherit" aria-label="menu">
+          <IconButton
+            onClick={toggleDrawer}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">{pageTitle}</Typography>
         </Toolbar>
       </AppBar>
-      <SwipeableDrawer open={isDrawerOpen} anchor="left" onClose={toggleDrawer} onOpen={toggleDrawer}>
-          <div onClick={toggleDrawer} onKeyDown={toggleDrawer}>
-            <List>
-              {routes.map(route => (
+      <SwipeableDrawer
+        open={isDrawerOpen}
+        anchor="left"
+        onClose={toggleDrawer}
+        onOpen={toggleDrawer}
+      >
+        <div onClick={toggleDrawer} onKeyDown={toggleDrawer}>
+          <List>
+            {routes.map((route) => (
               <ListItem disablePadding key={route[0]}>
                 <ListItemButton onClick={onRouteClick} href={route[0]}>
                   <ListItemText primary={route[1]} />
                 </ListItemButton>
-              </ListItem>))}
-            </List>
-          </div>
-        </SwipeableDrawer>
-      </React.Fragment>);
-}
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </SwipeableDrawer>
+    </React.Fragment>
+  );
+};
