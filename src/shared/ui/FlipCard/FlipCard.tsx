1b1d56d03e2b10ac2cb85d4ead3a7fd1f3a91298
SlavaL413 <slavlt2020@gmail.com>
2023-05-15 17:00:46 +0300

    flipcard rotate #1

--- src/shared/ui/FlipCard/FlipCard.tsx
+++ src/shared/ui/FlipCard/FlipCard.tsx
@@ -1,10 +1,34 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useReducer, useCallback, useState } from 'react';
 import { FlashCard, FlashCardProps } from '../FlashCard';
 import { reduceFlashCard, flip } from './reducer';
+import styled from '@emotion/styled';
+import { keyframes, css } from '@emotion/react';
 
 export interface FlipCardProps extends FlashCardProps {
     onFlip?: () => void
 }
+
+const rotateAnimation = keyframes`
+0% {
+  transform: rotateY(0deg);
+}
+50% {
+  transform: rotateY(90deg);
+}
+100% {
+  transform: rotateY(0deg);
+}
+`
+
+const StyledCard = styled.div`
+${(props: FlipCardProps) => props.side === "front" && css`
+  animation: ${rotateAnimation} 2s linear;
+`}
+
+${(props: FlipCardProps) => props.side === "back" && css`
+  animation: ${rotateAnimation} 2s linear;
+`}
+`
 
 export const FlipCard = ({ onFlip = () => {}, ...props }: FlipCardProps) => {
   const [side, dispatch] = useReducer(reduceFlashCard, props.side);
@@ -13,5 +37,9 @@ export const FlipCard = ({ onFlip = () => {}, ...props }: FlipCardProps) => {
     onFlip();
  }, []);
 
-  return <FlashCard onClick={doFlip} {...props} side={side} />;
+  return (
+    <StyledCard {...props}>
+      <FlashCard  onClick={doFlip} {...props} side={side} />
+    </StyledCard>
+  );
 };
