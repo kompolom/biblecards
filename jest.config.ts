09976c8fb16fcd157ec8dc782cb88d4b5821eda9
Evgeniy Baranov <master@kompolom.ru>
2023-11-12 22:57:08 +0300

    setup tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,18 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+    rootDir: 'src',
     preset: "ts-jest",
-    testEnvironment: "jsdom",
+    testEnvironment: "jest-environment-jsdom",
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+        "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
+        '^shared/(.*)$': '<rootDir>/shared/$1',
+        '^entities/(.*)$': '<rootDir>/entities/$1',
     },
     transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                'tsconfig': '<rootDir>/tsconfig.json',
-            },
-        ],
+        '^.+\\.tsx?$': "ts-jest"
     },
-    roots: [
-        "<rootDir>/src"
-    ],
 }
 
 export default config;
--- src/shared/ui/FlipCard/FlipCard.test.tsx
+++ src/shared/ui/FlipCard/FlipCard.test.tsx
@@ -1,18 +1,25 @@
+import '@testing-library/jest-dom'
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import { FlipCard } from './FlipCard';
 
 it('should change side on click', () => {
+    jest.useFakeTimers();
     const { queryByText, container } = render(<FlipCard frontContent="a" backContent="b" />);
     expect(queryByText("a")).toBeInTheDocument();
     fireEvent.click(container.firstChild);
+    act(() => jest.runAllTimers());
     expect(queryByText("b")).toBeInTheDocument();
+    jest.useRealTimers();
 });
 
 it('should trigger onFlip event', () => {
     const cb = jest.fn();
+    jest.useFakeTimers();
     const { container } = render(<FlipCard onFlip={cb} frontContent="a" backContent="b" />);
     fireEvent.click(container.firstChild);
+    act(() => jest.runAllTimers());
 
-    expect(cb).toBeCalled();
+    expect(cb).toHaveBeenCalled();
+    jest.useRealTimers();
 });
