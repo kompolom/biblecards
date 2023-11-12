bcb9050baf57056d4e5c3c0bd99601792944ee41
Evgeniy Baranov <master@kompolom.ru>
2023-11-12 19:19:19 +0300

    Loader: grid

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,25 +1,23 @@
-import React, { memo, useMemo } from 'react';
+import React, { memo } from 'react';
 import Box from '@mui/material/Box';
 import './loader.css';
 import type { SxProps } from '@mui/material';
+import clsx from 'clsx';
 
 export interface LoaderProps {
-    size?: string; 
-    colorscheme?: 'light'|'dark'
-    sx?: SxProps
+  colorscheme?: 'light' | 'dark';
+  sx?: SxProps;
 }
 
 export const Loader = memo((props: LoaderProps) => {
-  const styles = useMemo(() => {
-    const styles = {};
-    if(props.size) {
-        styles['--blockSize'] = props.size;
-    }
-    return styles;
-  }, [props]);
-
   return (
-    <Box sx={props.sx} style={styles} className="Loader">
+    <Box
+      sx={props.sx}
+      className={clsx('Loader', {
+        Loader_colorscheme_light: props.colorscheme === 'light',
+        Loader_colorscheme_dark: props.colorscheme === 'dark',
+      })}
+    >
       <span className="Loader-Block"></span>
       <span className="Loader-Block"></span>
       <span className="Loader-Block"></span>
