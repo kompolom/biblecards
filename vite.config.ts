38237e51c84c1bd0ce1d5567a39770317df76a2b
Evgeniy Baranov <master@kompolom.ru>
2026-06-02 17:04:16 +0300

    setup devcontainer

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    base: '',
-    plugins: [react({
-        jsxImportSource: '@emotion/react',
-        babel: {
-            plugins: ['@emotion/babel-plugin']
-        }
-    }), viteTsconfigPaths()],
-    server: {    
-        open: true,
-        port: 3000, 
-    },
-})
+  base: '',
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    viteTsconfigPaths(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    open: false,
+    port: 3000,
+  },
+});
