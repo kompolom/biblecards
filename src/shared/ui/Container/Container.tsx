e40c55c200677b77a31addc8ff02f6d5c6a88081
Evgeniy Baranov <master@kompolom.ru>
2026-06-21 12:03:57 +0000

    Refactor Container to use CSS tokens

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -1,11 +1,17 @@
 import { Box, styled } from '@mui/material';
+import { cssToken } from '../../css-token';
 
-export const Container = styled(Box)({
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    boxSizing: 'content-box',
-    containerType: 'inline-size',
-    maxWidth: 'var(--container--width, 100%)',
-    paddingInlineStart: 'var(--container--offset)',
-    paddingInlineEnd: 'var(--container--offset)',
+export const ContainerTokens = {
+  Width: cssToken('container', 'dimension', 'width'),
+  Offset: cssToken('container', 'dimension', 'offset'),
+};
+
+export const Container = styled(Box, { name: 'container' })({
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  boxSizing: 'border-box',
+  containerType: 'inline-size',
+  maxWidth: ContainerTokens.Width('100%'),
+  paddingInlineStart: ContainerTokens.Offset(),
+  paddingInlineEnd: ContainerTokens.Offset(),
 });
